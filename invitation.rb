42eab62ddf37266a17941bb16ed7834a5acb463d
akgmarquez <akgmarquez@gmail.com>
2018-07-10 17:42:28 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,16 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+guest_name = Hermione
+
+party_name = Best Halloween Party
+
+date = October 31
+
+time = 6 pm
+
+host_name = Harry Potter
+
+print Dear #{guest_name},
+
